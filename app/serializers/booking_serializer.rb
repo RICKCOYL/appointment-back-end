be250b574d345a4c40ebfc54ab94162c2d5c0faf
RICKCOYL <rickoburu@gmail.com>
2021-07-01 16:00:30 +0300

    Fix linter errors

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,6 +1,4 @@
-# rubocop:disable Layout/EndOfLine
 class BookingSerializer < ActiveModel::Serializer
   attributes :id, :title, :date, :time, :details
   belongs_to :user
 end
-# rubocop:enable Layout/EndOfLine
