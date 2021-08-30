dfdf1c36183e8e0a558d258c28fcc164d04e8f89
RICKCOYL <rickoburu@gmail.com>
2021-08-30 15:45:15 +0300

    Add evn for production

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,6 +1,6 @@
 class JsonWebToken
   # secret to encode and decode token
-  SECRET = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base
+  HMAC_SECRET = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
     payload[:exp] = exp.to_i
