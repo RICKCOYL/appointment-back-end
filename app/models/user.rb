da77666fa8135ab13629aabcdcf0f897a0fdac70
RICKCOYL <rickoburu@gmail.com>
2021-09-01 10:12:56 +0300

    fix linting errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :username, :email
 
-  VALID_EMAIL_REGEX = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  VALID_EMAIL_REGEX = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i.freeze
 
   validates :email, presence: true, uniqueness: { case_sensetive: false },
                     format: { with: VALID_EMAIL_REGEX, multiline: true }
